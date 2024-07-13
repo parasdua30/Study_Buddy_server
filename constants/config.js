cbf5e56c91241df5083f2ec3e260d6a523ad365a
Paras Dua <parasdua1130@gmail.com>
2024-07-13 19:38:15 +0530

    fixed

--- constants/config.js
+++ constants/config.js
@@ -3,7 +3,7 @@ const corsOptions = {
         "http://localhost:5173",
         "http://localhost:4173",
         process.env.CLIENT_URL,
-        "https://study-buddy-beige.vercel.app",
+        "https://study-buddy-client.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
